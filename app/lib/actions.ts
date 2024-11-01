2ce7b80d7f99e0ae3670f40f6b9f6b3b7ea8cbee
game-LP <ruudkrijgers@gmail.com>
2024-11-01 16:42:17 -0400

    handl error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,10 +29,16 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -45,19 +51,34 @@ export async function updateInvoice(id: string, formData: FormData) {
     status: formData.get("status"),
   });
   const amountInCents = amount * 100;
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    return {
+      message: "Database error, failed to update Invoice.",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`
+  throw new Error("THrowing error");
+
+  try {
+    await sql`
         DELETE FROM invoices WHERE id = ${id}
     `;
-  revalidatePath("/dashboard/invoices");
+    revalidatePath("/dashboard/invoices");
+    return { message: "Deleted Invoices" };
+  } catch (error) {
+    return {
+      message: "Database error, failed to delete Invoice.",
+    };
+  }
 }
